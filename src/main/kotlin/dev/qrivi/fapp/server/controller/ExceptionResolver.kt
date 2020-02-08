6f79e20bf16840154c9dd0f63be98c645be5147b
Kristof Dewilde <git@krivi.be>
2020-02-08 18:15:51 +0100

    Added custom 404 handling rather than Spring Boot's default

--- src/main/kotlin/dev/qrivi/fapp/server/controller/ExceptionResolver.kt
+++ src/main/kotlin/dev/qrivi/fapp/server/controller/ExceptionResolver.kt
@@ -2,18 +2,25 @@ package dev.qrivi.fapp.server.controller
 
 import dev.qrivi.fapp.server.dto.res.Response
 import dev.qrivi.fapp.server.dto.res.error.BadRequest
+import dev.qrivi.fapp.server.dto.res.error.NotFound
 import dev.qrivi.fapp.server.service.MessageService
 import dev.qrivi.fapp.server.util.generateResponse
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.servlet.NoHandlerFoundException
 
 @ControllerAdvice
 class ExceptionResolver(private val messages: MessageService) {
 
     @ExceptionHandler
-    fun handleTransactionSystemException(e: HttpMessageNotReadableException): ResponseEntity<Response> {
+    fun handleHttpMessageNotReadableException(e: HttpMessageNotReadableException): ResponseEntity<Response> {
         return generateResponse(BadRequest(error = messages["fapp.exception.HttpMessageNotReadableException.message"]))
+    }
+
+    @ExceptionHandler
+    fun handleNoHandlerFoundException(e: NoHandlerFoundException): ResponseEntity<Response> {
+        return generateResponse(NotFound(error = messages["fapp.exception.NoHandlerFoundException.message"]))
     }
 }
